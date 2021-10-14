d6d3356950e46edb448a9f89f6e8ceb0bde20126
Alexis Colin <nesquimo@gmail.com>
2021-10-14 12:16:07 +0200

    Add errorWrapper func

--- index.js
+++ index.js
@@ -9,11 +9,23 @@ let DATA = {
 };
 
 /**
+ * ERROR Wrapper
+ */
+
+async function fetchWrapper(url, cb) {
+    const res = await fetch(url)
+    if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+    } else {
+        return await cb(res)
+    }   
+}
+
+/**
  * HOURS API (what time is it?)
  */
 async function getTime() { 
-    await fetch(`https://api.jaunebleu.co/Get/hour`)
-        .then(res => res.text())
+    await fetchWrapper(`https://api.jaunebleu.co/Get/hour`, res => res.text())
         .then(res => {
             const regex = /T[0-9]{2}/g;
             const hour = parseFloat(res.match(regex)[0].substring(1));
@@ -23,27 +35,25 @@ async function getTime() {
                 : hour >= 12 ? 'noon' 
                 : hour >= 6 ? 'morning' 
                 : 'night';
-        })
+        }).catch(e => console.log(e));
 };
 
 /**
  * WEATHER API (weather & temperature)
  */
 async function getWeather() { 
-    await fetch(`https://api.jaunebleu.co/Get/weather`)
-        .then(res => res.json())
+    await fetchWrapper(`https://api.jaunebleu.co/Get/weather`, res => res.json())
         .then(res => {  
             DATA.weather = res.weather[0].description;
             DATA.temperature = Math.round(res.main.temp);
-        })
+        }).catch(e => console.log(e));
 };
 
 /**
  * TRAFFIC API (subway state)
  */
 async function getTraffic() { 
-    await fetch(`https://api.jaunebleu.co/Get/subway`)
-        .then(res => res.json())
+    await fetchWrapper(`https://api.jaunebleu.co/Get/subway`, res => res.json())
         .then(res => {
             const wording = {
                 normal: "OK",
@@ -52,28 +62,26 @@ async function getTraffic() {
             };
 
             DATA.traffic = wording[res.result.slug] || wording['nok'];
-        })
+        }).catch(e => console.log(e));
 };
 
 /**
  * SPOTIFY API (last listening)
  */
 async function getMusic() { 
-    await fetch(`https://api.jaunebleu.co/Get/music`)
-        .then(res => res.json())
+    await fetchWrapper(`https://api.jaunebleu.co/Get/music`, res => res.json())
         .then(res => {
             const mainArtist = res.artist.split(', ')[0]; // get main artist onl
             DATA.music = res.track_name.charAt(0).toUpperCase() + res.track_name.slice(1); // capitalize first letter
             DATA.artist = mainArtist.charAt(0).toUpperCase() + mainArtist.slice(1); // same
-        })
+        }).catch(e => console.log(e));
 };
 
 /**
  * POCKET API (last read articles)
  */
 async function getPocketArticles() {
-    await fetch(`https://api.jaunebleu.co/Get/pocket?count=10`)
-        .then(res => res.json())
+    await fetchWrapper(`https://api.jaunebleu.co/Get/pocket?count=10`, res => res.json())
         .then(res =>{
             DATA.readings = Object.values(res.list)
                 .filter(art => art.tags && art.tags.hasOwnProperty('dev')) //dev tag only
@@ -87,7 +95,7 @@ async function getPocketArticles() {
                         website: art.given_url.match(/^https?\:\/\/(?!(?:www\.)?(?:youtube\.com|youtu\.be))([^\/:?#]+)(?:[\/:?#]|$)/i)[1].replace(/^www\./,'')
                     }
                 })
-        })
+        }).catch(e => console.log(e));
     }
 
 /**
